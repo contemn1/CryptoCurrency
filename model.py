45be622abab058306193df963e942964939637f9
contemn1 <xunjiez614218@gmail.com>
2018-04-24 20:15:16 -0400

    bug fix

---
+++ model.py
@@ -0,0 +1,135 @@
+from torch import nn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+import copy
+
+class Predictor(object):
+    def __init__(self, model, train_data, test_data, batch_size=64, use_cuda=False, l2_reg=0.001):
+        self.use_cuda = use_cuda
+        self.model = model.cuda() if use_cuda else model
+        self.batch_size = batch_size
+        self.training_set = TensorDataset(data_tensor=train_data["x"], target_tensor=train_data["y"])
+        self.test_set = TensorDataset(data_tensor=test_data["x"], target_tensor=test_data["y"])
+        self.train_loader = self.init_dataloader(self.training_set)
+        self.test_loader = self.init_dataloader(self.test_set)
+        self.loss_fn = nn.MSELoss().cuda() if use_cuda else nn.MSELoss()
+        self.optimizer = optim.Adam(self.model.parameters(),
+                                    weight_decay=l2_reg)
+
+    def init_dataloader(self, dataset):
+        return DataLoader(dataset=dataset,
+                          batch_size=self.batch_size,
+                          pin_memory=self.use_cuda,
+                          shuffle=False)
+
+    def train_epoch(self):
+        self.model.train()
+        all_costs = []
+        for x_batch, y_batch in self.train_loader:
+            x_batch = x_batch.view(x_batch.size(1), x_batch.size(0), -1)
+            # forward
+            if self.use_cuda:
+                x_batch = x_batch.cuda()
+                y_batch = y_batch.cuda()
+
+            x_batch = Variable(x_batch)
+            y_batch = Variable(y_batch)
+            output = self.model(x_batch)
+            # loss
+            loss = self.loss_fn(output, y_batch)
+            loss_data = loss.data.cpu()[0] if self.use_cuda else loss.data[0]
+            all_costs.append(loss_data)
+            # backward
+            self.optimizer.zero_grad()
+            loss.backward()
+            # Update parameters
+            self.optimizer.step()
+
+        return np.mean(all_costs)
+
+    def calculate_validation_cost(self, dev_loader=None):
+        self.model.eval()
+        if not dev_loader:
+            dev_loader = self.test_loader
+
+        all_costs = []
+        for x_batch, y_batch in dev_loader:
+            x_batch = x_batch.view(x_batch.size(1), x_batch.size(0), -1)
+            # forward
+            if self.use_cuda:
+                x_batch = x_batch.cuda()
+                y_batch = y_batch.cuda()
+
+            x_batch = Variable(x_batch)
+            y_batch = Variable(y_batch)
+            output = self.model(x_batch)
+            loss = self.loss_fn(output, y_batch)
+            loss_data = loss.data.cpu()[0] if self.use_cuda else loss.data[0]
+            all_costs.append(loss_data)
+
+        return np.mean(all_costs)
+
+    def predict(self, dev_loader):
+        self.model.eval()
+        yhat = np.array([])
+        for Xbatch, _ in dev_loader:
+            Xbatch = Xbatch.view(Xbatch.size(1), Xbatch.size(0), -1)
+            if self.use_cuda:
+                Xbatch = Xbatch.cuda()
+
+            Xbatch = Variable(Xbatch, volatile=True)
+            output = self.model(Xbatch)
+            yhat = np.append(yhat,
+                             output.data.cpu().numpy())
+        yhat = np.vstack(yhat)
+        return yhat
+
+    def fit(self, epoches, maximum_failure):
+        train_cost = []
+        dev_cost = []
+        num_failed = 0
+        best_cost = 1
+        for _ in range(epoches):
+            current_train_cost = self.train_epoch()
+            current_valid_cost = self.calculate_validation_cost()
+            if current_valid_cost < best_cost:
+                best_cost = current_valid_cost
+                best_model = copy.deepcopy(self.model)
+                num_failed = 0
+            else:
+                num_failed += 1
+
+            if num_failed > maximum_failure:
+                break
+
+            train_cost.append(current_train_cost)
+            dev_cost.append(current_valid_cost)
+
+        self.model = best_model
+
+
+
+class LSTMRegressor(nn.Module):
+    def __init__(self, input_size, hidden_size, dropout_rate=0.5):
+        super(LSTMRegressor, self).__init__()
+        self.rnn = nn.LSTM(input_size=input_size, hidden_size=hidden_size)
+        self.init_rnn()
+        self.dropout_layer = nn.Dropout(dropout_rate)
+        self.linear = nn.Linear(hidden_size, 1)
+
+    def forward(self, data):
+        _, (last_hidden, _) = self.rnn(data)
+        last_hidden = last_hidden[0]
+        dropped_out_hidden = self.dropout_layer(last_hidden)
+        result = self.linear(dropped_out_hidden)
+        return result
+
+    def init_rnn(self):
+        for name, param in self.rnn.named_parameters():
+            if 'bias' in name:
+                nn.init.constant(param, 0.0)
+            elif 'weight' in name:
+                nn.init.xavier_normal(param)
---
+++ preprocessing.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from torch.utils.data import TensorDataset
+import torch
+from model import LSTMRegressor
+from model import Predictor
+import copy
+from matplotlib import pyplot
+
+def read_file(data_path):
+    data = pd.read_csv(filepath_or_buffer=data_path, index_col="Date")
+    return data
+
+
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - look_back):
+        a = dataset[i:(i + look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+
+    return np.array(dataX, dtype=np.float32), np.array(dataY, dtype=np.float32)
+
+
+def create_data_dict(dataset, look_back=1):
+    trainX, trainY = create_dataset(dataset, look_back)
+
+    return {"x": torch.from_numpy(trainX).view(trainX.shape[0], trainX.shape[1], 1),
+            "y": torch.from_numpy(trainY).view(trainY.shape[0], 1)}
+
+
+if __name__ == '__main__':
+    a = read_file("/Users/zxj/Downloads/bitcoin-price-prediction/bitcoin_price_Training - bitcoin_price.2013Apr-2017Aug.csv.csv")
+    a = a[::-1]
+    a['Close'].replace(0, np.nan, inplace=True)
+    a['Close'].fillna(method='ffill', inplace=True)
+    values = a['Close'].values.reshape(-1, 1)
+    values = values.astype('float32')
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    train_size = int(len(scaled) * 0.8)
+    test_size = len(scaled) - train_size
+    train, test = scaled[0:train_size, :], scaled[train_size:len(scaled), :]
+    train_dict = create_data_dict(train, 5)
+    test_dict = create_data_dict(test, 5)
+    model = LSTMRegressor(input_size=1,
+                          hidden_size=128,
+                          dropout_rate=0.5)
+    predictor = Predictor(model=model,
+                          train_data=train_dict,
+                          test_data=test_dict)
+
+    predictor.fit(150, 15)
+    results = predictor.predict(predictor.test_loader).reshape(1, -1)[0]
+    test_y = test_dict["y"].numpy().reshape(1, -1)[0]
+    pyplot.plot(results, label='predict')
+    pyplot.plot(test_y, label='true')
+    pyplot.legend()
+    pyplot.show()
