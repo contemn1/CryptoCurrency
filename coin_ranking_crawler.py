2d9f02d452a2dfa268c9e9ec307841332a1fda4b
contemn1 <xunjiez614218@gmail.com>
2018-04-25 07:21:37 -0400

    change coin_cap crawler

--- coin_ranking_crawler.py
+++ coin_ranking_crawler.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import logging
+from datetime import datetime, timedelta
+import re
 
 headers = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36"}
+
 
 def fetch_url(url, header_dict=headers, record_error=None):
     try:
@@ -14,3 +17,27 @@ def fetch_url(url, header_dict=headers, record_error=None):
         if record_error is not None:
             record_error(url)
         return ""
+
+
+def parse(html_page):
+    soup = BeautifulSoup(html_page, 'html.parser')
+    res = []
+    for news_blocks in soup.find_all("tr", attrs={"class": "text-right"}):
+        row = []
+        children = news_blocks.findChildren()
+        for ele in children:
+            row.append(ele.text)
+        row[0] = datetime.strptime(row[0], "%b %d, %Y").strftime("%Y-%m-%d")
+        row[1:] = [re.sub(",", "", num) for num in row[1:]]
+        res.append(row)
+
+    res = res[::-1]
+    return res
+
+if __name__ == '__main__':
+    url = "https://coinmarketcap.com/currencies/bitcoin-cash/historical-data/?start=20170425&end=20180425"
+    page = fetch_url(url)
+    result = parse(page)
+    print("\t".join(["Date", "Open", "High", "Low", "Close", "Volume", "Market Cap"]))
+    for ele in result:
+        print("\t".join(ele))
